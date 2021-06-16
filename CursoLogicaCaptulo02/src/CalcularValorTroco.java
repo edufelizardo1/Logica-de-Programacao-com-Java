32575de57b0763ea48d3acc836b6a85e0b035efa
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-16 17:20:27 -0300

    calculadora

--- CursoLogicaCaptulo02/src/CalcularValorTroco.java
+++ CursoLogicaCaptulo02/src/CalcularValorTroco.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
 
 public class CalcularValorTroco {
 	public static void main(String[] args) {
-		System.out.println("Olá mundo!");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um valor de produto: ");
+		
+		Double ValorProduto = sc.nextDouble();
+		
+		System.out.print("Digite um valor de valor passado pelo cliente: ");
+		
+		Double ValorPassadoPeloCliente = sc.nextDouble();
+		
+		
+		Double resultado = ValorPassadoPeloCliente - ValorProduto;
+		
+		System.out.println("Valor de troco: " + resultado);
+		
+		sc.close();
 	}
 }
