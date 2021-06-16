89ad796955c502bfbd42ab1215428c28fb130621
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-16 18:01:22 -0300

    NomeSobrenome

---
+++ CursoLogicaCaptulo02/src/modulo3/NomeSobrenome.java
@@ -0,0 +1,18 @@
+package modulo3;
+
+import java.util.Scanner;
+
+public class NomeSobrenome {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Nome: ");
+		String nome = sc.nextLine();
+		
+		System.out.print("Sobrenome: ");
+		String sobrenome = sc.nextLine();
+		
+		System.out.println("Seu nome completo é " + nome + " " + sobrenome);
+	}
+}
