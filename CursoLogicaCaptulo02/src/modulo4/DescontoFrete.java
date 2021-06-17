f66cb184aad37cf58f3d256b9341e259097b7a9a
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-06-17 13:51:33 -0300

    Update DescontoFrete

--- CursoLogicaCaptulo02/src/modulo4/DescontoFrete.java
+++ CursoLogicaCaptulo02/src/modulo4/DescontoFrete.java
@@ -1,5 +1,28 @@
 package modulo4;
 
+import java.util.Scanner;
+
 public class DescontoFrete {
 
+	static final double FRETE = 15;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("Valor do produto: R$");
+		double vl = sc.nextDouble();
+		
+		boolean frete = vl >= 100;
+		
+		if(frete) {
+			System.out.println("Valor total: R$" + vl);
+		} else {
+			System.out.println("Valor + Frete: R$" + (vl + FRETE));
+		}
+		
+		sc.close();
+		
+	}
 }
